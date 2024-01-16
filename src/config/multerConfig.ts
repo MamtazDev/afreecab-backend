151ab544131636d98df62791d68add47a6d0aad2
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-16 12:39:23 +0600

    some changes

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -1,16 +1,38 @@
 import multer from "multer";
+import fs from "fs";
 import path from "path";
 
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/images");
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
+    }
+
+    if (!fs.existsSync("public/images")) {
+      fs.mkdirSync("public/images");
+    }
+
+    cb(null, "public/images");
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
   },
 });
 
-const upload = multer({ storage: storage, fileFilter: function (req, file, cb) {
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
     var ext = path.extname(file.originalname);
 
     if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
@@ -21,7 +43,16 @@ const upload = multer({ storage: storage, fileFilter: function (req, file, cb) {
   },
   limits: {
     fileSize: 3 * 1024 * 1024,
-  }, });
-
+  },
+});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/assets");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
 
 export default upload;
